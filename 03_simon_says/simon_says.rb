0ef9e03cf54e6f535d0ba37c23240cc21feeab2f
jcarrharris <joncarrharris@gmail.com>
2013-08-26 10:10:49 -0400

    Added problems

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -12,14 +12,37 @@ def repeat(str, num = 2)
 	return result1.chomp(' ')
 end
 
-def start_of_word(str, num = 0)
+def start_of_word(str, num=0)
 	array = str.split("")
-	if num = 1
+	if num == 1
 		array.first
-	elsif num = 2 
-		result = array.first + array[2]
-		return result
+	elsif num == 2 
+		array[0..1].join("")
+	else 
+		array[0..num -1].join("")
 	end
+end
+
+def first_word(str)
+		array = str.split(" ")
+		array.first
+end
+
+#Ask for help 
+def titleize(str)
+	str = %w{a an the and but or for nor of}
+	words = string.split
+	result = ""
+	i = 1
+
+	result << words.first.capitalize
+	while i < words.length
+		x = words[i].to_s.capitalize
+		result << " " << x
+		i += 1
+	end
+	return result.strip
+
 end
 
 
