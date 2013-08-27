1364c93b5662172c86d5087d9d3f31fc75c52e7e
jcarrharris <joncarrharris@gmail.com>
2013-08-27 15:36:07 -0400

    adds

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,2 +1,19 @@
-def vowel (word)
+VOWELS = %w{a e i o u }
+
+# def translate (word)
+# 	vowel = "#{word}" + "ay" unless word =! VOWELS 
+# 	modified_word = 
+	
+# end
+
+def translate(word)
+	if VOWELS.include?(word[0])
+		vowel = word + word[0] + "y"
+		return vowel
+	elsif !VOWELS.include?(word[0]) && !VOWELS.include?(word[1]) 
+		two_consonants = word + word[0] + "ay"
+		return two_consonants[2..-1]
+	else consonant = word + word[0] + "ay"
+			return consonant[1..-1]
+	end
 end
